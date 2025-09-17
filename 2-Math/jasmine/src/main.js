4ddcee12ba4c3359b7630d0f078a0690e48de9c9
Jack Mossberg <jackmossberg25@gmail.com>
2025-09-17 10:56:47 -0500

    Finished assignment 2

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,2 +1,14 @@
+// task 1
+let hourlyRate = 60;
+let dayRate = hourlyRate * 8;
 
-  
+// task 2
+let budget = 1000;
+let workdays = Math.floor(budget / dayRate);
+
+// task 3
+let totalNumberOfBillableDays = 35;
+let discountedMonthlyRate = 22 * dayRate * 0.8;
+let fullMonths = Math.floor(totalNumberOfBillableDays / 22);
+let remainingDays = totalNumberOfBillableDays % 22;
+let totalProjectCost = Math.ceil((fullMonths * discountedMonthlyRate) + (remainingDays * dayRate));
