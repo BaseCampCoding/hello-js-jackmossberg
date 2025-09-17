b9675ab69e47c6810193e149462631cc15f18a0d
Jack Mossberg <jackmossberg25@gmail.com>
2025-09-17 15:47:11 -0500

    Update main.js

--- 6-Conditionals/jasmine/src/main.js
+++ 6-Conditionals/jasmine/src/main.js
@@ -4,8 +4,8 @@ if (kind == "car" || kind == "truck") needsLicenseResult = true
 
 let option1 = "Volvo";
 let option2 = "Ford";
-
-let chooseVehicleResult = option1 + " is clearly the better choice."
+let options = [option1, option2].sort()
+let chooseVehicleResult = options[0] + " is clearly the better choice."
 
 let originalPrice = 11550.0
 let age = 2.0
