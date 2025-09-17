de53218cda50ff8e34d8d4fa709cb839bd1aecd9
Jack Mossberg <jackmossberg25@gmail.com>
2025-09-17 15:45:19 -0500

    Update main.js

---
+++ 6-Conditionals/jasmine/src/main.js
@@ -0,0 +1,20 @@
+let kind = "car";
+let needsLicenseResult = false;
+if (kind == "car" || kind == "truck") needsLicenseResult = true
+
+let option1 = "Volvo";
+let option2 = "Ford";
+
+let chooseVehicleResult = option1 + " is clearly the better choice."
+
+let originalPrice = 11550.0
+let age = 2.0
+
+let calculateResellPriceResult = null;
+if (age < 3.0) {
+    calculateResellPriceResult = originalPrice * 0.8;
+} else if (age > 10.0) {
+    calculateResellPriceResult = originalPrice * 0.5;
+} else {
+    calculateResellPriceResult = originalPrice * 0.7;
+}
